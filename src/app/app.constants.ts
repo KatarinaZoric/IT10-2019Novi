acf682acfab0cd53ae364bbf00ba39c266fe93b9
KatarinaZoric <73577469+KatarinaZoric@users.noreply.github.com>
2022-08-30 21:07:57 +0200

    v2

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,6 +1,6 @@
 
-export const KLIJENT_URL= 'http://localhost:8083/klijent';
-export const KREDIT_URL= 'http://localhost:8083/kredit';
-export const RACUN_URL='http://localhost:8083/racun';
-export const TIPRACUNA_URL='http://localhost:8083/tipRacuna';
-export const RACUNI_ZA_KLIJENTA_URL='http://localhost:8083/racunZaKlijentaId';
+export const KLIJENT_URL= 'https://kacarva2221.herokuapp.com/klijent';
+export const KREDIT_URL= 'https://kacarva2221.herokuapp.com/kredit';
+export const RACUN_URL='https://kacarva2221.herokuapp.com/racun';
+export const TIPRACUNA_URL='https://kacarva2221.herokuapp.com/tipRacuna';
+export const RACUNI_ZA_KLIJENTA_URL='https://kacarva2221.herokuapp.com/racunZaKlijentaId';
